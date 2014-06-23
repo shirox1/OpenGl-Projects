e3f81c02883173b170ba731f6d595591ea9c327e
Max <shirox1@msn.com>
2014-06-23 16:27:40 +0100

    Change version of glew
    changed from 1.9.0 to 1.10.0 version of glew and moved the dll to the
    right directory so everything compiles

--- OpenGL-tutorial/src/main.cpp
+++ OpenGL-tutorial/src/main.cpp
@@ -4,41 +4,55 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
+//static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+//  if(key == GLFW_KEY_A && action == GLFW_PRESS)
+//    printf("a has been pressed\n");
+//}
+
 int main()
 {
-  GLFWwindow* window;
-
-  //init GLFW
+  // initialize GLFW
   if(!glfwInit())
   {
-    fprintf(stderr, "Failed to initialize GLFW");
+    fprintf(stderr, "Failed to initialize GLFW.\n");
     return -1;
   }
 
-  /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
+  // Create a windowed mode window and its OpenGL context
+  GLFWwindow* window;
+  window = glfwCreateWindow(1024, 768, "Hello World", NULL, NULL);
+  if(!window)
+  {
+    fprintf(stderr, "Failed to open GLFW window.\n");
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  // Make the window's context current
+  glfwMakeContextCurrent(window); //initialize GLEW
+  glewExperimental = true; // Needed in core profile
+
+  // initilize glew
+  if(glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW.\n");
+    return -1;
+  }
+
+  //glfwSetKeyCallback(window, key_callback);
+
+  /* Loop until the user closes the window */
+  while(!glfwWindowShouldClose(window))
+  {
+    /* Render here */
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
 
